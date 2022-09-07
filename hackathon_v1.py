0ba95213f0be981c5c02d145bec4783dc8b6ef84
Antoine <antoine.olivier112@enpc.eleves.fr>
2022-09-07 10:14:21 +0200

    Faut modifier les noms de paths

--- hackathon_v1.py
+++ hackathon_v1.py
@@ -12,6 +12,7 @@ for item in object_list:
     if item['classTitle'] == 'People':
         boxes_list.append(item['points']['exterior'])
         
+#Return a list of boxes  in [x0,y0,x1,y1] format
 def get_boxes(json_file):
     df = pd.read_json(json_file, orient='index')
 
@@ -214,6 +215,34 @@ def afficher_boxes_json(
     plt.yticks([])
     plt.show()
 
+#def critere
+
+
+def prop_people_detected(
+        img_path, threshold=0.5
+        ):
+    
+    boxes_person = []
+    
+    # On recupere la liste des personnes identifiees par l'algo
+    boxes, pred_cls = get_prediction(img_path, threshold)
+    
+    for i in range(len(boxes)):
+        if pred_cls[i] == "person":
+            boxes_person.append(boxes[i])
+    
+    
+    # On recupere la liste des personnes reelles
+    df = pd.read_json(img_path+".json", orient='index')
+    object_list = df.values[3][0]
+    real_boxes_list = []
+    
+    for item in object_list:
+        if item['classTitle'] == 'People':
+            real_boxes_list.append(item['points']['exterior'])
+            # real_boxes_list contient donc tous les rectangles voulus
+    
+
 
 # %% https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
 
@@ -259,7 +288,7 @@ def bb_intersection_over_union(boxA, boxB):
 #%% Definition of the dataset ,marche pas pour l'instant
 import torch
 import os
-class PennFudanDataset(torch.utils.data.Dataset):
+class WorksiteDataset(torch.utils.data.Dataset):
     def __init__(self, root, transforms):
         self.root = root
         self.transforms = transforms
@@ -307,31 +336,59 @@ class PennFudanDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.imgs)
 
-#def critere
+#%%
+from engine import train_one_epoch, evaluate
+import utils
 
 
-def prop_people_detected(
-        img_path, threshold=0.5
-        ):
-    
-    boxes_person = []
-    
-    # On recupere la liste des personnes identifiees par l'algo
-    boxes, pred_cls = get_prediction(img_path, threshold)
-    
-    for i in range(len(boxes)):
-        if pred_cls[i] == "person":
-            boxes_person.append(boxes[i])
-    
-    
-    # On recupere la liste des personnes reelles
-    df = pd.read_json(img_path+".json", orient='index')
-    object_list = df.values[3][0]
-    real_boxes_list = []
-    
-    for item in object_list:
-        if item['classTitle'] == 'People':
-            real_boxes_list.append(item['points']['exterior'])
-            # real_boxes_list contient donc tous les rectangles voulus
-    
+def main():
+    # train on the GPU or on the CPU, if a GPU is not available
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
+    # our dataset has two classes only - background and person
+    num_classes = 2
+    # use our dataset and defined transformations
+    dataset = WorksiteDataset('Detection_Train_Set', get_transform(train=True))
+    dataset_test = WorksiteDataset('Detection_Test_Set', get_transform(train=False))
+
+    # split the dataset in train and test set
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset = torch.utils.data.Subset(dataset, indices[:-50])
+    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+    # define training and validation data loaders
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=2, shuffle=True, num_workers=4,
+        collate_fn=utils.collate_fn)
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, shuffle=False, num_workers=4,
+        collate_fn=utils.collate_fn)
+
+    # get the model using our helper function
+    model = get_model_instance_segmentation(num_classes)
+
+    # move model to the right device
+    model.to(device)
+
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,
+                                momentum=0.9, weight_decay=0.0005)
+    # and a learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
+
+    # let's train it for 10 epochs
+    num_epochs = 10
+
+    for epoch in range(num_epochs):
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        evaluate(model, data_loader_test, device=device)
+
+    print("That's it!")
